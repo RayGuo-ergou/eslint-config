09a88cfe3110a8a84c285c47137a89aec82085af
Ray Guo <ma2808203259@hotmail.com>
2023-10-09 23:03:08 +1100

    feat: add ignored files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 // eslint.config.js
 import antfu from '@antfu/eslint-config'
 
-// TODO: customization
-export default antfu()
+// Export the config function in case I have to customize it
+export { antfu as ray }
+
+// Default export is the config should use in most of my projects
+export default antfu(
+  { ignores: ['./fixtures', 'components.d.ts', 'nuxt.d.ts', 'dist', '.nuxt', 'node_modules', '.output'] },
+)
