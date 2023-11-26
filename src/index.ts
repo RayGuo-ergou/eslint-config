60f0098e12ec40c8ea07f26db6787b0247b4c24d
Ray Guo <ma2808203259@hotmail.com>
2023-11-27 00:02:55 +1100

    feat: export default config

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ import antfu from '@antfu/eslint-config'
 // Export the config function in case I have to customize it
 export { antfu as ray }
 
+// Default config
+export const defaultConfig: Parameters<typeof antfu>[0] = {
+  ignores: ['./fixtures', 'components.d.ts', 'nuxt.d.ts', 'dist', '.nuxt', 'node_modules', '.output'],
+}
+
 // Default export is the config should use in most of my projects
-export default antfu(
-  { ignores: ['./fixtures', 'components.d.ts', 'nuxt.d.ts', 'dist', '.nuxt', 'node_modules', '.output'] },
-)
+export default antfu(defaultConfig)
