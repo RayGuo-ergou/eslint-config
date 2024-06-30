53e3536b9a775ffb72cbc636493bd65cc721b339
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-06-30 17:11:13 +1000

    chore: bump deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,7 @@
+// TODO:typescript config file support in in rfcs stage
+// Can remove after that been published
+// See https://github.com/eslint/rfcs/pull/117
+// https://github.com/eslint/eslint/pull/18134
 import JITI from 'jiti'
 
 const jiti = JITI(import.meta.url)
